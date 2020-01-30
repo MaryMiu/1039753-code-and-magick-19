0f1f4a4bd0a8a70ac1c323a7373ce8fea372ad69
MaryMiu <m.zhivilo@gmail.com>
2020-01-30 22:16:31 +0700

    Копирует stat.js из предыдущей ветки

--- js/stat.js
+++ js/stat.js
@@ -10,19 +10,20 @@ var GISTOGRAMMA_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TEXT_HEIGHT = 25;
-var BAR_HEIGHT = GISTOGRAMMA_HEIGHT - TEXT_HEIGHT * 2;
 var TEXT_COLOR = '#000000';
 var TEXT_FONT = '16px PT Mono';
 var TEXT_BASELINE = 'hanging';
-var TEXT_X = CLOUD_X + CLOUD_GAP;
-var TEXT_Y = CLOUD_Y + CLOUD_GAP;
-var TEXT_Y2 = CLOUD_Y + CLOUD_GAP + TEXT_HEIGHT;
-var TIME_Y = CLOUD_Y + BAR_GAP * 1.7;
-var NAME_Y = TIME_Y + GISTOGRAMMA_HEIGHT;
 var BLUE_H = 255;
 var BLUE_S = 100;
 var BLUE_L = 50;
-var COLOR_ME = 'rgba(255, 0, 0, 1)';
+var COLOR_DEFAULT = 'rgba(255, 0, 0, 1)';
+var barHeight = GISTOGRAMMA_HEIGHT - TEXT_HEIGHT * 2;
+var textX = CLOUD_X + CLOUD_GAP;
+var textY = CLOUD_Y + CLOUD_GAP;
+var textY2 = CLOUD_Y + CLOUD_GAP + TEXT_HEIGHT;
+var timeY = CLOUD_Y + BAR_GAP * 1.7;
+var nameY = timeY + GISTOGRAMMA_HEIGHT;
+
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -39,10 +40,11 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var createText = function (ctx, color, font, baseline) {
+var renderText = function (ctx, color, font, baseline, text, coordX, coordY) {
   ctx.fillStyle = color;
   ctx.font = font;
   ctx.textBaseline = baseline;
+  ctx.fillText(text, coordX, coordY);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -50,22 +52,18 @@ window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  createText(ctx, TEXT_COLOR, TEXT_FONT, TEXT_BASELINE);
-  ctx.fillText('Ура вы победили!', TEXT_X, TEXT_Y);
-  createText(ctx, TEXT_COLOR, TEXT_FONT, TEXT_BASELINE);
-  ctx.fillText('Список результатов:', TEXT_X, TEXT_Y2);
+  renderText(ctx, TEXT_COLOR, TEXT_FONT, TEXT_BASELINE, 'Ура вы победили!', textX, textY);
+  renderText(ctx, TEXT_COLOR, TEXT_FONT, TEXT_BASELINE, 'Список результатов:', textX, textY2);
 
   for (var i = 0; i < names.length; i++) {
     var barX = CLOUD_X + CLOUD_GAP + (BAR_WIDTH + BAR_GAP) * i;
-    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    var barY = GISTOGRAMMA_HEIGHT - barHeight + TIME_Y - TEXT_HEIGHT;
-    ctx.fillText(Math.floor(times[i]), barX, TIME_Y);
-    ctx.fillStyle = 'hsl(' + BLUE_H + ',' + BLUE_S * Math.random() + '%,' + BLUE_L + '%)';
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = COLOR_ME;
-    }
-    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+    var barDynamicHeight = (barHeight * times[i]) / maxTime;
+    var barY = GISTOGRAMMA_HEIGHT - barDynamicHeight + timeY - TEXT_HEIGHT;
+    var colorRandom = 'hsl(' + BLUE_H + ',' + BLUE_S * Math.random() + '%,' + BLUE_L + '%)';
+    ctx.fillText(Math.floor(times[i]), barX, timeY);
+    ctx.fillStyle = (names[i] === 'Вы') ? COLOR_DEFAULT : colorRandom;
+    ctx.fillRect(barX, barY, BAR_WIDTH, barDynamicHeight);
     ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(names[i], barX, NAME_Y);
+    ctx.fillText(names[i], barX, nameY);
   }
 };
